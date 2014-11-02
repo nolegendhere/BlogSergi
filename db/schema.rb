9784676ac9f3bb2280e7c0e53b089c417922b1b2
Sergi <sergigarciapena@gmail.com>
2014-11-02 12:16:25 +0100

    fixing the server from local machine

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813171616) do
+ActiveRecord::Schema.define(version: 20141029185519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calendar_month_years", force: true do |t|
+    t.datetime "month_post"
+    t.datetime "year_post"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -30,7 +37,7 @@ ActiveRecord::Schema.define(version: 20140813171616) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
