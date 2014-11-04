2082fad8a667c2b21d2fefcbca2e85b5f64c5ea3
Sergi <sergigarciapena@gmail.com>
2014-11-04 11:22:59 +0100

    migrate in production

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20141103163034) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
