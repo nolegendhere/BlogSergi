574d1b4a1386bcef9b87bbaaada0000adcc9a985
Sergi <sergigarciapena@gmail.com>
2014-11-05 18:05:08 +0100

    comments ASC

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :user
 	
-	default_scope -> { order('created_at DESC') }
+	default_scope -> { order('created_at ASC') }
 	validates :content, presence: true, length: { minimum: 5 }
 	validates :user_id, presence: true
 	validates :post_id, presence: true
