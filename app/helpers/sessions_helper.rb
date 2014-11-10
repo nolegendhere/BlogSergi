7baab9d1653fa9c9247b88c49417ca79748bd998
Sergi <sergigarciapena@gmail.com>
2014-11-10 11:33:43 +0100

    without cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,22 +1,50 @@
 module SessionsHelper
+
+#with cookies
+=begin
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
     self.current_user = user
   end
-  
+
+  def current_user
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
+=end
+
+#without cookies
+#=begin
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def sign_out
+    session.delete(:user_id)
+      @current_user = nil
+  end
+#=end  
   def signed_in?
     !current_user.nil?
   end
   
    def current_user=(user)
     @current_user = user
-  end
-
-  def current_user
-    remember_token = User.digest(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
   end
   
   def current_user?(user)
@@ -31,12 +59,6 @@ module SessionsHelper
     end
   end
 
-  def sign_out
-    current_user.update_attribute(:remember_token,
-                                  User.digest(User.new_remember_token))
-    cookies.delete(:remember_token)
-    self.current_user = nil
-  end
   
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   
   before_save { email.downcase! }
-  before_create :create_remember_token
+  #with cookies
+  #before_create :create_remember_token
   
   validates :name, presence: true, length: { maximum: 30 }
   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,6 +14,22 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
   
+ 
+  
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Post.where("user_id = ?", id)
+  end
+
+#without cookies
+  def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+  end
+
+#with cookies
+=begin
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
@@ -21,16 +38,11 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
   
-  def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Post.where("user_id = ?", id)
-  end
-  
-
   private
 
     def create_remember_token
       self.remember_token = User.digest(User.new_remember_token)
     end
+=end
 
 end
