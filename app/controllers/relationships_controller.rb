9e3252c0cd6e8f449a2e1568e5ed0a98c1e1b325
Sergi <sergigarciapena@gmail.com>
2014-11-12 19:46:29 +0100

    ajaxfollowunfollow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,15 +2,21 @@ class RelationshipsController < ApplicationController
   before_action :signed_in_user
 
   def create
-    post = Post.find(params[:post_id])
-    current_user.subscription.follow(post)
-    redirect_to post
+    @post = Post.find(params[:post_id])
+    current_user.subscription.follow(@post)
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.js
+    end
   end
 
   def destroy
-    post = Relationship.find(params[:id]).post
-    current_user.subscription.unfollow(post)
-    redirect_to post
+    @post = Relationship.find(params[:id]).post
+    current_user.subscription.unfollow(@post)
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.js
+    end
   end
 
 
